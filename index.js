234f069dc7903c78b9f4a93f2eca27c25b08e788
unknown <you@example.com>
2023-06-08 11:11:58 -0500

    users added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const cors=require('cors');
 require('dotenv').config()
 const app=express();
+const jwt=require('jsonwebtoken')
 const port=process.env.PORT||5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
@@ -32,13 +33,36 @@ async function run() {
 
 
     const classCollection = client.db("summerDb").collection("classes");
+    const usersCollection = client.db("summerDb").collection("users");
 
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.send({ token })
+    })
 
     app.get('/classes', async (req, res) => {
         const result = await classCollection.find().toArray();
         res.send(result);
       })
+      app.get('/users',  async (req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+      });
+      app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUser = await usersCollection.findOne(query);
+  
+        if (existingUser) {
+          return res.send({ message: 'user already exists' })
+        }
+  
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
