99741a952c557a31f162df85384475e6398e7e2f
unknown <you@example.com>
2023-06-12 11:36:34 -0500

    cart delete added

--- index.js
+++ index.js
@@ -192,7 +192,14 @@ async function run() {
     const result=await cartsCollection.insertOne(item)
     res.send(result)
    })
+ 
 
+  app.delete('/carts/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query={_id:new ObjectId(id)}
+    const result=await cartsCollection.deleteOne(query);
+    res.send(result)
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
