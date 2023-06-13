16d47f701c3aa92bc4bf4bf1222ffbec84d75dc3
unknown <you@example.com>
2023-06-13 12:29:28 -0500

    carts

--- index.js
+++ index.js
@@ -202,10 +202,10 @@ async function run() {
   })
  
 
-  app.get('/carts/:id', async(req, res) => {
+  app.get('/carts/:id',verifyJWT, async(req, res) => {
     const id = req.params.id;
     const query = {_id: new ObjectId(id)}
-    const result = await myCollection.findOne(query);
+    const result = await cartsCollection.findOne(query);
     res.send(result);
   })
 
