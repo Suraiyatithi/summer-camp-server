16a37e29a477e4310855766c6c93cf162f7317f7
unknown <you@example.com>
2023-06-13 10:19:46 -0500

    cart id getting

--- index.js
+++ index.js
@@ -200,6 +200,14 @@ async function run() {
     const result=await cartsCollection.deleteOne(query);
     res.send(result)
   })
+ 
+
+  app.get('/carts/:id', async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await myCollection.findOne(query);
+    res.send(result);
+  })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
